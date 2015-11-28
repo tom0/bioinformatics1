4542e125ca595dd67ad16970925df887a58580ec
Tom <tom.sisk@outlook.com>
2015-11-28 13:06:51 +0800

    pattern count challenge

---
+++ pattern_count/pattern_count.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+
+
+def pattern_count(text, pattern):
+    count = 0
+    pattern_len = len(pattern)
+    for i in range(0, len(text) - pattern_len):
+        if text[i:i + pattern_len] == pattern:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    import sys
+
+    t, p = sys.argv[1], sys.argv[2]
+    print(pattern_count(t, p))
